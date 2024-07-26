b27694c196e987c79ca98080ce6eb727da19711f
myin98 <dnjsdydpf@gmail.com>
2024-07-26 09:48:57 +0900

    Test disable add

--- src/main/java/com/app/controller/WebController.java
+++ src/main/java/com/app/controller/WebController.java
@@ -1,15 +1,19 @@
 package com.app.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebController {
     
+	@Autowired
+	private TestMapper tm;
+	
     @GetMapping("/")
     public String home(){
-        return "환영합니다.";
+        return "환영합니다 (" + tm.testDB() + ")";
     }
 
 }
